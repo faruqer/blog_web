8fb4f4eda19e8f1baa00ab1c94576a3e6b334392
faruqer <osamafaruq4444@gmail.com>
2023-12-16 05:56:24 +0000

    code formalizing

--- index.js
+++ index.js
@@ -5,12 +5,27 @@ const mongoose = require("mongoose");
 
 
 const app = express()
-mongoose.connect("mongodb+srv://faruq:faruq120910@cluster0.qnzsgnz.mongodb.net/userDB", {useNewUrlParser: true})
+// mongoose.connect("mongodb+srv://faruq:faruq120910@cluster0.qnzsgnz.mongodb.net/userDB", {useNewUrlParser: true})
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
 let posts=[{
+    author:"osama",
+    title:"test3",
+    content:"dosgvsidhdsisgsjd",
+    date:"4-87"
+}, {
+    author:"kebe",
+    title:"test2",
+    content:"dosgvsidhdsisgsjdajsvsjgshs sjsusb ssjvss ss siss. d sis d dgdddhus s snssksbsndhd shsj i.   j. dbsjs. dbs shs shsksudbwkd f",
+    date:"4-37"
+},{
+    author:"abe",
+    title:"test1",
+    content:"dosgvsidhdsisgsjd",
+    date:"2-34"
+},{
     author:"osama",
     title:"test3",
     content:"dosgvsidhdsisgsjd",
@@ -47,9 +62,47 @@ let isAuthonticated=false
 app.get("/", (req, res)=>{
     res.render("root")
 })
+
 app.get("/signup", (req, res) => {
     res.render("sign_up", { message: false });
 })
+
+app.get("/signin", (req, res) => {
+    res.render("sign_in", {message:false})
+})
+
+app.get("/home", (req, res)=>{
+    if(isAuthonticated){
+    res.render("home", {posts:posts}) 
+    }else{
+        res.redirect("/signin")
+    }
+})
+
+app.get("/history", (req, res)=>{
+    if (isAuthonticated){
+        res.render("history")
+    }else{
+        res.redirect("signin")
+    }
+})
+
+app.get("/account", (req, res)=>{
+    if (isAuthonticated){
+        res.render("account")
+    }else{
+        res.redirect("signin")
+    }
+})
+
+app.get("/post", (req, res)=>{
+    if(isAuthonticated){
+        res.render("post")
+    }else{
+        res.redirect("/signin")
+    }
+})
+
 
 app.post("/signup", (req, res) => {
     const userName = req.body.userName
@@ -83,45 +136,34 @@ app.post("/signup", (req, res) => {
     
 })
 
-app.get("/signin", (req, res) => {
-    res.render("sign_in", {message:false})
-});
+
 
 app.post("/signin", (req, res) => {
     const userName = req.body.uname
     const password = req.body.password
-    User.find()
-        .then((users)=>{
-            for (let i=0; i<users.length; i++){
-                if (users[i].userName===userName){
-                   if(users[i].password===password){
+    // User.find()
+    //     .then((users)=>{
+    //         for (let i=0; i<users.length; i++){
+    //             if (users[i].userName===userName){
+    //                if(users[i].password===password){
                     
-                        isAuthonticated=true
-                        res.redirect("/home")
-                   } 
-                }
-            }
-            res.render("sign_in", {message:"Incorrect user name or password"})
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+    //                     isAuthonticated=true
+    //                     res.redirect("/home")
+    //                } 
+    //             }
+    //         }
+    //         res.render("sign_in", {message:"Incorrect user name or password"})
+    //     })
+    //     .catch((err)=>{
+    //         console.log(err)
+    //     })
+    if (userName=="c" && password==1)
+{
+    isAuthonticated=true
+    res.redirect("/home")
+}
 })
 
-app.get("/home", (req, res)=>{
-    if(isAuthonticated){
-    res.render("home", {posts:posts}) 
-    }else{
-        res.redirect("/signin")
-    }
-})
-app.get("/post", (req, res)=>{
-    if(isAuthonticated){
-        res.render("post")
-    }else{
-        res.redirect("/signin")
-    }
-})
 
 app.post("/post", (req, res)=>{
     posts.push(req.body.post)
